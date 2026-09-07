e491357c7f1d9dee93ff42d21e2015b9f39e8d79
zhoubinjia <hinnka@muchuntech.com>
2026-09-07 17:55:29 +0800

    bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.hinnka.mycamera"
         minSdk = 30
         targetSdk = 36
-        versionCode = 151
-        versionName = "1.27.2.3"
+        versionCode = 152
+        versionName = "1.28.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
